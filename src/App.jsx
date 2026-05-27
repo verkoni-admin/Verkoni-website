8500c7b896708cd3dd5c0aaad6de62efc592e54f
Nagesh kumbar <nageshkumbar70@gmail.com>
2026-05-27 14:17:21 +0530

    router configured, header logo updated, scroll to top update added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
+      <ScrollToTop/>
       <header>
         <Navbar/>
       </header>
       <main>
         <Outlet/>
       </main>
-      <Footer />
+      <footer>
+        <Footer />
+      </footer>
     </>
   )
 }
